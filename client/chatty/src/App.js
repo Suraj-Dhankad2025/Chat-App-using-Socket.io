5a7792203672999747d7a1457152c3b4f0fe6a9d
suraj dhankad <94227272+Suraj-Dhankad2025@users.noreply.github.com>
2023-08-03 17:33:35 +0530

    changes added

--- client/chatty/src/App.js
+++ client/chatty/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import { nanoid } from "nanoid";
-import io from 'socket.io-client';
+import io from "socket.io-client";
 import { useState, useEffect } from "react";
 const socket = io.connect("http://localhost:5500");
 const userName = nanoid(4);
 function App() {
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
- 
+
   const sendChat = (e) => {
     e.preventDefault();
     socket.emit("chat", {message, userName});
@@ -16,7 +16,6 @@ function App() {
   useEffect(() => {
     socket.on("chat", (payload) => {
       setChat([...chat, payload]);
-      io.emit("chat", payload);
     });
   })
   return (
@@ -26,7 +25,7 @@ function App() {
         {chat.map((payload, index) => {
           return (
             <p key={index}>{payload.message}:<span>id:{payload.userName}</span></p>
-            )
+            );
         })}
         <form onSubmit={sendChat}>
           <input
